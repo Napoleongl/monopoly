34794c2f6aabf3a546ea51d9cf804554f47018cb
Napoleongl <guslu389@student.liu.se>
2021-01-06 01:45:07 +0100

    added verbose-flag for printing retain old position to check for GO-passage only check owners for lots added break for missing balance

--- R/main.R
+++ R/main.R
@@ -1,10 +1,11 @@
-# ============ Imports ================
+# ============ Imports ====================
 library(tidyverse)
 library(magrittr)
 source("R/board_functions.R")
 source("R/player_functions.R")
 
-# ============ Set up =================
+# ============ Set up =====================
+verbose = TRUE
 players <- create_players(c("p1", "p2"), 20)
 board <- create_board()
 
@@ -12,12 +13,15 @@ board <- create_board()
 current_player_id <- 0L
 nplayers <- nrow(players)
 while(TRUE){
+  old_position   <- players %>% get_position(current_player_id)
+  # TODO - attempt unprison before move!
   players %<>% make_move(current_player_id)
-  new_position <- players %>% get_position(current_player_id)
-  position_owner <- board %>% get_board_field(new_position, "owner")
-  position_type <- board %>% get_board_field(new_position, "type")
-
+  new_position   <- players %>% get_position(current_player_id)
+  position_type  <- board %>% get_board_field(new_position, "type")
+  
+  # ============ Lots a' logic ==============
   if(position_type == "lot"){
+    position_owner <- board %>% get_board_field(new_position, "owner")
     if(position_owner == current_player_id){ #Own lot
       #End turn
     } else if(is.na(position_owner)){ #Empty buyable lot
@@ -26,19 +30,29 @@ while(TRUE){
       board %<>% change_lot_owner(current_player_id, new_position)
       players %<>% 
         change_balance(current_player_id, -1 * lot_price) 
-      }
+      } else { break } # Needs balance > 1 after buying lot
     } else if(position_owner != current_player_id){ #Lot owned buy someone else - pay rent!
       lot_price <- board %>% get_board_field(new_position, "price")
       double_rent <- board %>% owns_whole_lot_group(position_owner, new_position)
       players %>% transfer_balance(position_owner, current_player_id, 
                                    lot_price * (1 + double_rent))
-    } else {stop("Lot owner not in (NA, current player or other player)!!!")}
+    } else {
+      stop("Lot owner not in (NA, current player or other player)!!!")
+    }
+ 
+  # ============ Prison logic ===============
   } else if(position_type == "prison"){
     # end turn
+    
+  # ============ Parking time ===============
   } else if(position_type == "parking"){
     #end_turn
+    
+  # ============ To prison ya go ============
   } else if(position_type == "to_prison"){
     player %<>% imprison(current_player_id)
+    
+  # ============ Back to the start ==========
   } else if(position_type == "go"){
     
   }
