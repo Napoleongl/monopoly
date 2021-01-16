f449a75f334f854c75b2194743c489d41a591452
Napoleongl <guslu389@student.liu.se>
2021-01-16 01:23:22 +0100

    added gift chance card

--- R/chance_card_functions.R
+++ R/chance_card_functions.R
@@ -6,6 +6,7 @@ draw_card <- function(player_id){
 # ============ Card types =============
 give_money_to_player
 get_money_from_all
+get_gift
 move_to_and_buy_or_pay
 move_to_and_steal
 get_out_of_prison
