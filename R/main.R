05093670f965f893d742cfe7289bf2a59287c98c
Napoleongl <guslu389@student.liu.se>
2021-01-16 01:24:00 +0100

    functionalized main loop added chance card lot type action holder

--- R/main.R
+++ R/main.R
@@ -6,55 +6,77 @@ source("R/player_functions.R")
 
 # ============ Set up =====================
 verbose = TRUE
-players <- create_players(c("p1", "p2"), 20)
-board <- create_board()
-
-# ============ Actual Game ================
-current_player_id <- 0L
-nplayers <- nrow(players)
-while(TRUE){
-  old_position   <- players %>% get_position(current_player_id)
-  # TODO - attempt unprison before move!
-  players %<>% make_move(current_player_id)
-  new_position   <- players %>% get_position(current_player_id)
-  position_type  <- board %>% get_board_field(new_position, "type")
-  
-  # ============ Lots a' logic ==============
-  if(position_type == "lot"){
-    position_owner <- board %>% get_board_field(new_position, "owner")
-    if(position_owner == current_player_id){ #Own lot
-      #End turn
-    } else if(is.na(position_owner)){ #Empty buyable lot
-      lot_price <- board %>% get_board_field(new_position, "price")
-      if(get_player_field(players, current_player_id, "balance") > lot_price){
-      board %<>% change_lot_owner(current_player_id, new_position)
-      players %<>% 
-        change_balance(current_player_id, -1 * lot_price) 
-      } else { break } # Needs balance > 1 after buying lot
-    } else if(position_owner != current_player_id){ #Lot owned buy someone else - pay rent!
-      lot_price <- board %>% get_board_field(new_position, "price")
-      double_rent <- board %>% owns_whole_lot_group(position_owner, new_position)
-      players %>% transfer_balance(position_owner, current_player_id, 
-                                   lot_price * (1 + double_rent))
-    } else {
-      stop("Lot owner not in (NA, current player or other player)!!!")
+max_rounds <- 100
+main <- function(nplayers, max_rounds, verbose){
+  players <- create_players(letters[1:nplayers], 22 - (2 * nplayers))
+  board <- create_board()
+  current_player_id <- 0L
+  rounds <- 0
+  # ============ Actual Game ================
+  while(rounds < (max_rounds * nplayers) -1){
+    old_position   <- players %>% get_position(current_player_id)
+    
+    if(players %>% get_player_field(current_player_id, "imprisoned")){          # Attempt unprison prior to move
+      players %<>%  unprison(current_player_id)
     }
- 
-  # ============ Prison logic ===============
-  } else if(position_type == "prison"){
-    # end turn
-    
-  # ============ Parking time ===============
-  } else if(position_type == "parking"){
-    #end_turn
-    
-  # ============ To prison ya go ============
-  } else if(position_type == "to_prison"){
-    player %<>% imprison(current_player_id)
-    
-  # ============ Back to the start ==========
-  } else if(position_type == "go"){
-    
+    if(!(players %>% get_player_field(current_player_id, "imprisoned"))){       # Only play turn if not imprisoned, else stay...
+      players %<>% make_move(current_player_id)
+      new_position   <- players %>% get_position(current_player_id)
+      if(new_position < old_position){ # I.e. player is on or has passed GO
+        players %<>% change_balance(current_player_id, 2) 
+      }
+      position_type  <- board %>% get_board_field(new_position, "type")
+      
+      # ============ Lots a' logic ==============
+      if(position_type == "lot"){
+        position_owner <- board %>% get_board_field(new_position, "owner")
+        if(is.na(position_owner)){                                              # Empty buyable lot
+          lot_price <- board %>% get_board_field(new_position, "price")
+          if(get_player_field(players, current_player_id, "balance") > lot_price){
+            board %<>% change_lot_owner(current_player_id, new_position)
+            players %<>% 
+              change_balance(current_player_id, -1 * lot_price) 
+          } else {                                                              # Needs balance > 1 after buying lot
+            if(verbose){
+              write(paste(current_player_id, "looses due to insufficient funds"),"")
+            }
+            break
+            } 
+        } else if(position_owner == current_player_id){                         # Own lot - End turn
+        } else if(position_owner != current_player_id){                         # Lot owned buy someone else - pay rent!
+          lot_price <- board %>% get_board_field(new_position, "price")
+          double_rent <- board %>% owns_whole_lot_group(position_owner, new_position)
+          players %>% transfer_balance(position_owner, current_player_id, 
+                                       lot_price * (1 + double_rent))
+        } else {
+          stop("Lot owner not in (NA, current player or other player)!!!")      # Shouldn't happen...
+        }
+        
+        # ============ Prison logic ===============
+      } else if(position_type == "prison"){
+        # just visiting, end turn
+        
+        # ============ Parking time ===============
+      } else if(position_type == "parking"){
+        # end_turn - its sole purpose...
+        
+        # ============ To prison ya go ============
+      } else if(position_type == "to_prison"){
+        players %<>% imprison(current_player_id)
+        
+        # ============ Back to the start ==========
+      } else if(position_type == "go"){
+        # end turn, payout is handled in make_move()
+        
+        # ============ Chance cards ===============
+      } else if(position_type == "chance"){
+        # TODO...
+      }
+    }
+    # ============ Round stat update ==========
+    if(any((players %>% pull(balance)) < 1)){ break }
+    current_player_id <- (current_player_id +1) %% nplayers
+    rounds <- rounds + 1
   }
+  end_game_stats(players, board, rounds==(max_rounds*nplayers))
 }
-
