e7742abb17fa64464c1790dbbfe12c409c0b49d5
Napoleongl <guslu389@student.liu.se>
2021-01-19 00:15:26 +0100

    removedverbosity from siulation call as printing is supressed in parLapply anyway. added options enable running simulation as script instead. removed error_checking as this isnt part of simulation

--- R/simulation.R
+++ R/simulation.R
@@ -1,12 +1,27 @@
-# ============ Imports ====================
+# ============ Script args ================
+suppressPackageStartupMessages(library("optparse"))
+
+option_list <- list( 
+  make_option(c("-c", "--cores"), type = "integer", default = parallel::detectCores()-1,
+              help = "Number of cores to use [default %default]",
+              dest = "ncores"),
+  make_option(c("-", "--rounds"), type = "integer", default = 100,
+              help = "Maximum turns for each player before calling it quits. [default %default]",
+              dest = "rounds"),
+  make_option(c("-p", "--players"), type = "integer", default = 2,
+              help = "Number of players in each game [default %default]",
+              dest = "nplayers"),
+  make_option(c("-g", "--games"), type = "integer", default = 100,
+              help = "Number of games played per core [default %default]",
+              dest = "ngames")
+)
+
+opt_parser <- OptionParser(option_list=option_list);
+opts       <- parse_args(opt_parser);
+# ============ Set up =====================
 library(parallel)
 
-error_round_finder  <-FALSE
-error_round <- 999
-end_game_browser <- FALSE
-verbose <- FALSE
-
-cl <- makeCluster(getOption("cl.cores", 4) - 1)
+cl <- makeCluster(opts$num_cores)
 
 clusterEvalQ(cl = cl, library(tidyverse))
 clusterEvalQ(cl = cl, library(magrittr))
@@ -14,16 +29,20 @@ clusterEvalQ(cl = cl, source("R/board_functions.R"))
 clusterEvalQ(cl = cl, source("R/player_functions.R"))
 clusterEvalQ(cl = cl, source("R/chance_card_functions.R"))
 clusterEvalQ(cl = cl, source("R/main.R"))
-clusterExport(cl = cl, c("error_round_finder", "error_round", "end_game_browser", "verbose"))
+clusterExport(cl = cl, c("opts"))
 
-
-games <- parLapply(cl = cl, 1:300, function(x){
-  print(x)
+# ============ Simulation =================
+games <- parLapply(cl = cl, 1L:(opts$ncores * opts$ngames), function(x){
   set.seed(x)
-  main(nplayers = 3, verbose = FALSE, max_rounds = 100)
+  main(nplayers = opts$nplayers, max_rounds = opts$rounds)
 })
 
 games_data <- bind_rows(games)
 
+save(games_data, "data/games_data.rdata")
+
+# error_round_finder  <-FALSE
+# error_round <- 999
+# end_game_browser <- FALSE
 # set.seed(156)
 # main(nplayers = 3, verbose = FALSE, max_rounds = 100)
