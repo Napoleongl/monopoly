f87d8b9c9f4fd96122df16a0c6794f4d7a9c9210
Napoleongl <guslu389@student.liu.se>
2021-01-19 00:34:45 +0100

    added function skeleton tocards

--- R/chance_card_functions.R
+++ R/chance_card_functions.R
@@ -1,17 +1,29 @@
 # ============ Chance cards ===========
 draw_card <- function(player_id){
-  return()
+  return(TRUE)
 }
 
 # ============ Card types =============
-give_money_to_player
-get_money_from_all
-get_gift
-move_to_and_buy_or_pay
-move_to_and_steal
-get_out_of_prison
+give_money_to_player <- function(.player, .board){
+  return(TRUE)
+}
+get_money_from_all <- function(.player, .board){
+  return(TRUE)
+}
+get_gift <- function(.player, .board){
+  return(TRUE)
+}
+move_to_and_buy_or_pay <- function(.player, .board){
+  return(TRUE)
+}
+move_to_and_steal <- function(.player, .board){
+  return(TRUE)
+}
+get_out_of_prison <- function(.player, .board){
+  return(TRUE)
+}
 
 # ============ Deck definition ========
 create_chance_deck <- function(){
-  
+  return(TRUE)
 }
