ddb32e8f7bb6c99f4f847359610945ec358c98d4
Napoleongl <guslu389@student.liu.se>
2021-01-19 09:34:39 +0100

    switch to pbapply lib for parallell instead and fixed issue with not specifying any file to save to

--- R/simulation.R
+++ R/simulation.R
@@ -5,44 +5,48 @@ option_list <- list(
   make_option(c("-c", "--cores"), type = "integer", default = parallel::detectCores()-1,
               help = "Number of cores to use [default %default]",
               dest = "ncores"),
-  make_option(c("-", "--rounds"), type = "integer", default = 100,
+  make_option(c("-r", "--rounds"), type = "integer", default = 100,
               help = "Maximum turns for each player before calling it quits. [default %default]",
               dest = "rounds"),
   make_option(c("-p", "--players"), type = "integer", default = 2,
               help = "Number of players in each game [default %default]",
               dest = "nplayers"),
-  make_option(c("-g", "--games"), type = "integer", default = 100,
+  make_option(c("-g", "--games"), type = "integer", default = 10,
               help = "Number of games played per core [default %default]",
-              dest = "ngames")
+              dest = "ngames"),
+  make_option(c("-s", "--savefile"), type = "character", default = "data/games_data.Rdata",
+              help = "Path to store simulation result [default %default]",
+              dest = "save_file")
 )
+
 
 opt_parser <- OptionParser(option_list=option_list);
 opts       <- parse_args(opt_parser);
+
 # ============ Set up =====================
-library(parallel)
+suppressPackageStartupMessages(library(pbapply))
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(magrittr))
 
-cl <- makeCluster(opts$num_cores)
-
-clusterEvalQ(cl = cl, library(tidyverse))
-clusterEvalQ(cl = cl, library(magrittr))
-clusterEvalQ(cl = cl, source("R/board_functions.R"))
-clusterEvalQ(cl = cl, source("R/player_functions.R"))
-clusterEvalQ(cl = cl, source("R/chance_card_functions.R"))
-clusterEvalQ(cl = cl, source("R/main.R"))
-clusterExport(cl = cl, c("opts"))
+source("R/board_functions.R")
+source("R/player_functions.R")
+source("R/chance_card_functions.R")
+source("R/main.R")
 
 # ============ Simulation =================
-games <- parLapply(cl = cl, 1L:(opts$ncores * opts$ngames), function(x){
-  set.seed(x)
-  main(nplayers = opts$nplayers, max_rounds = opts$rounds)
-})
+total_games <- opts$ncores * opts$ngames
+write(paste("Simulating", total_games, "games with", opts$nplayers, 
+            "players using", opts$ncores, "threads."),
+      "")
+
+games <- pblapply(X = 1L:total_games, cl = opts$ncores, FUN = function(x){
+                    set.seed(x)
+                    main(nplayers = opts$nplayers, max_rounds = opts$rounds)
+                  })
 
 games_data <- bind_rows(games)
 
-save(games_data, "data/games_data.rdata")
-
-# error_round_finder  <-FALSE
-# error_round <- 999
-# end_game_browser <- FALSE
-# set.seed(156)
-# main(nplayers = 3, verbose = FALSE, max_rounds = 100)
+save(games_data, opts, file = opts$save_file)
+write(paste0("Simulation done, results saved to ", getwd(), "/",opts$save_file),
+      "")
+# ============ Finished ===================
