3e3d032fe538c0abae19f889fdf6b9e1c91e6bd6
Gustav <gustav.n.lundberg@outlook.com>
2021-01-21 12:27:28 +0100

    defined all cards except move_To_and_steal and cleaned up process of picking card

--- R/chance_card_functions.R
+++ R/chance_card_functions.R
@@ -1,59 +1,58 @@
-# ============ Chance cards ===========
-pick_card <- function(.chance_deck, type = "random", ...){
-  # Simulates picking a card from the deck of chance cards. 
-  # Type is either "random" for any possible card or the name of a card function.
-  # If type is specified then addition card arguments like amount or lot group
-  # are supplied via "...".
-  
-  return(card)
-}
-
-# ============ Card types =============
-give_money_to_player <- function(.players, .board, player_id, ...){
+create_deck <- function(){
+  list(
+# ============ Player cards ===========
+give_gift_to_player = function(.players, .board, player_id, amount = NULL){
   # The player is instructed to give an amount to another (random) player.
   # Potentially game ending for the current player.
-  
-  #TODO: Check player balance before transfer!
-  amount <- 2
+  if(is.null(amount)){ amount <- 2 }
   other_player <- filter(.players, ID != player_id) %>% 
     pull(ID) %>% 
     sample(1)
   if(verbose %in% c("all", "chance")){
     write(paste("Player", player_id, "gives", amount, "money to player", other_player), "")
   }
-  .players %<>% transfer_balance(other_player, player_id, +2)
+  .players %<>% transfer_balance(other_player, player_id, amount)
   return(list(players = .players, board = .board))
-}
+},
 
-get_gift_from_all <- function(.players, .board, player_id, ...){
+get_gift_from_all = function(.players, .board, player_id, amount = NULL){
   # The player gets a gift from all other players.
   # Potentially game ending for other player with balance <= gift amount.
 
-  #TODO: check balances and loop transfers. Unsure how to both break and decrease balances...
-  amount <- 1
+  if(is.null(amount)){ amount <- 1 }
   other_players <- filter(.players, ID != player_id) %>% pull(ID) 
-  liquid_players <- filter(.players, ID != player_id && balance > amount) %>% pull(ID) 
   for(p in other_players){ 
     .players %<>% transfer_balance(player_to = player_id, player_from = p, amount = amount)
   }
-  game_alive <- identical(other_players, liquid_players)                        # all players have enough balance
-  bankrupt_players <- setdiff(other_players, liquid_players)
   if(verbose %in% c("all", "chance")){
     write(paste("Player", player_id, "gets",amount, "money from every player!"), "")
     }
-  return(list(players = .players, board = .board, game_alive = game_alive))
-}
+  return(list(players = .players, board = .board))
+},
 
-get_gift_from_bank <- function(.players, .board, player_id, ...){
+get_gift_from_bank = function(.players, .board, player_id, amount = NULL){
   # The player gets a gift from the bank.
+  if(is.null(amount)){ amount <- 2 }
   if(verbose %in% c("all", "chance")){
-    write(paste("Player", player_id, "gets a gift of 2 money!"), "")
+    write(paste("Player", player_id, "gets a gift of", amount, "money!"), "")
   }
   .players %<>% change_balance(player_id, +2) 
   return(list(players = .players, board = .board))
-}
+},
 
-move_to_and_buy_or_pay <- function(.players, .board, player_id, ...){
+get_out_of_jail_card = function(.players, .board, player_id){
+  # Player gets a card that lets them out of jail without bail. Card is kept
+  # until needed.
+  if(verbose %in% c("all", "chance")){
+    write(paste("Player", player_id, "gets a get out of jail card!"), "")
+  }
+  .players %<>% set_player_field(player_id = player_id, method = "set", 
+                                 field = "get_out_of_jail_card", value = TRUE)
+  return(list(players = .players, board = .board))
+},
+
+# ============ Board cards ============
+move_to_and_buy_or_pay = function(.players, .board, player_id, groups = NULL){
   # The player is instructed to go to any lot of group x or y and if it is vacant
   # buy it, else pay rent to the owner (or do nothing if self owned).
   # The only place where there is actual strategy in the game as you need to 
@@ -68,13 +67,15 @@ move_to_and_buy_or_pay <- function(.players, .board, player_id, ...){
     write(paste("Player", player_id, ""), "")
     }
   return(list(players = .players, board = .board))
-}
+},
 
-move_to_and_steal <- function(.players, .board, player_id, ...){
+move_to_and_steal = function(.players, .board, player_id, lot_id){
   # The player is instructed to move to a specific lot and buy it regardless
   # of whether it is already owned or not. 
   # Potentially game ending if the player has balance <= lot price.
-  lot_id <- .board %>% filter(type == "lot") %>% pull(ID) %>% sample(1)
+  if(is.null(lot_id)){
+    lot_id <- .board %>% filter(type == "lot") %>% pull(ID) %>% sample(1)
+  }
   lot_price <- get_board_field(.board, lot_id, "price")
   previous_owner <- get_board_field(.board, lot_id, "owner")
   if(get_player_field(.players, player_id, "balance") < lot_price){             # Player hasn't got enough funds
@@ -96,27 +97,33 @@ move_to_and_steal <- function(.players, .board, player_id, ...){
   }
   return(list(players = .players, board = .board))
 }
+)}
+# ============ Spare card =============
+# .blank_card_template <- function(.players, .board, player_id, ...){
+#   #In case I find some more cards!
+#   if(verbose %in% c("all", "chance")){
+#     write(paste("Player", player_id, ""), "")
+#     }
+#   return(list(players = .players, board = .board))
+# }
 
-get_out_of_jail_card <- function(.players, .board, player_id, ...){
-  # Player gets a card that lets them out of jail without bail. Card is kept
-  # until needed.
-  if(verbose %in% c("all", "chance")){
-    write(paste("Player", player_id, "gets a get out of jail card!"), "")
-  }
-  .players %<>% set_player_field(player_id = player_id, method = "set", 
-                                 field = "get_out_of_jail_card", value = TRUE)
-  return(list(players = .players, board = .board))
-}
-
-.blank_card_template <- function(.players, .board, player_id, ...){
-  #In case I find some more cards!
-  if(verbose %in% c("all", "chance")){
-    write(paste("Player", player_id, ""), "")
+# ============ Pick a card, any card! =
+pick_card <- function(card_types, type = "random", card_probs = NULL, ...){
+  # Simulates picking a card from the deck of chance cards. 
+  # Type is either "random" for any possible card or the name of a card function.
+  # If type is specified then addition card arguments like amount or lot group
+  # are supplied via "...".
+  if(type == "random"){
+    if(is.null(card_probs)){
+      card_probs <- c(move_to_and_buy_or_pay     = 4, get_gift_from_all     = 2,
+                      get_gift_from_bank      = 4, get_out_of_jail_card  = 4,
+                      give_gift_to_player  = 0, move_to_and_steal     = 4)
+    } else {
+      card_probs <- card_probs[names(card_types)] # sorting necessary since sample() doesn't honor names of prob-vector vs names(x)
     }
-  return(list(players = .players, board = .board))
-}
-
-# ============ Deck definition ========
-create_chance_deck <- function(){
-  return(TRUE)
+    card <- sample(card_types, 1, prob  = card_types)
+  } else {
+    card <- card_types[[type]]
+  }
+  return(card)
 }
